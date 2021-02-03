c9fb116e546dac45ad7ffbb8b791cde510caf8a8
solygambas <solygambas@gmail.com>
2021-02-03 10:36:34 +0100

    add tailwind config

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Tailwind CSS
 
+A recipe website to understand Tailwind CSS basics.
+
 Based on [Tailwind CSS Tutorial](https://www.youtube.com/playlist?list=PL4cUxeGkcC9gpXORlEHjc5bgnIi5HEGhw) by Shaun Pelling - The Net Ninja (2020).
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ff6363",
+        secondary: {
+          100: "#e2e2d5",
+          200: "#883883",
+        },
+      },
+      fontFamily: {
+        body: ["Nunito", "sans-serif"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};

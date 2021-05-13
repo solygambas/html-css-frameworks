9ce8ea6687d188597116864438533b20151ced7d
solygambas <51904909+solygambas@users.noreply.github.com>
2021-05-13 15:56:10 +0200

    add product info

--- ninja-coffee/index.js
+++ ninja-coffee/index.js
@@ -5,3 +5,20 @@ const navBarMenu = document.querySelector("#nav-links");
 burgerIcon.addEventListener("click", () =>
   navBarMenu.classList.toggle("is-active")
 );
+
+// tabs
+const tabs = document.querySelectorAll(".tabs li");
+const tabContentBoxes = document.querySelectorAll("#tab-content > div");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach((item) => item.classList.remove("is-active"));
+    tab.classList.add("is-active");
+    const target = tab.dataset.target;
+    tabContentBoxes.forEach((box) => {
+      box.getAttribute("id") === target
+        ? box.classList.remove("is-hidden")
+        : box.classList.add("is-hidden");
+    });
+  });
+});

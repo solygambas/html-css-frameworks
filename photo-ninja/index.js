c10a8f4598b775cb09d5d68ee8892d860f605008
solygambas <51904909+solygambas@users.noreply.github.com>
2021-05-14 18:56:01 +0200

    add parallax

--- photo-ninja/index.js
+++ photo-ninja/index.js
@@ -1,5 +1,7 @@
 const sidenav = document.querySelector(".sidenav");
 const lightboxes = document.querySelectorAll(".materialboxed");
+const parallaxes = document.querySelectorAll(".parallax");
 
 M.Sidenav.init(sidenav);
 M.Materialbox.init(lightboxes);
+M.Parallax.init(parallaxes);
